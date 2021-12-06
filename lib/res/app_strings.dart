9f40ce160cdbc97168e9c209602a2b2dc83a3e5e
Sergey Kolpakov <eltexnika2008@yandex.ru>
2021-12-06 16:35:28 +0500

    some updates

---
+++ lib/res/app_strings.dart
@@ -0,0 +1,12 @@
+abstract class AppStrings {
+
+  static const String title = 'Flutter WeatherApp';
+  static const String enterCityName = 'Введите название города';
+  static const String hintText = 'Город';
+  static const String confirmSelection = 'Подтвердить выбор';
+  static const String otherCity = 'Выбрать другой город';
+  static const String error = 'Ошибка получения данных';
+  static const String temp = 'Температура ';
+  static const String humidity = 'Влажность ';
+  static const String windSpeed = 'Скорость ветра ';
+}
